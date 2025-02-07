d2c57a3229127d5af38224ccd1a5c2c86cf11620
Ricardo Costa da Silva <rcsilva2405@gmail.com>
2025-02-07 11:07:03 -0500

    Github pages

--- game-api/src/env.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-const envSchema = z.object({
-    GAME_API_BASE_URL: z.string().url(),
-    API_KEY: z.string(),
-    PORT: z.coerce.number().default(3333),
-});
-
-export const env = envSchema.parse(process.env);
